f218f91a2ddc7aa5806a7642c4c1d74fa549a3a6
Gwendal <gwendalminguy@outlook.fr>
2026-04-17 09:22:51 +0200

    Add entry point

--- markdown2html.py
+++ markdown2html.py
@@ -7,18 +7,24 @@ Script generating HTML from Markdown syntax.
 import os
 import sys
 
-if len(sys.argv) != 3:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-    sys.exit(1)
 
-input_file_name = sys.argv[1]
-input_file_path = os.path.abspath(input_file_name)
+def main():
+    if len(sys.argv) != 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.exit(1)
 
-output_file_name = sys.argv[2]
-output_file_path = os.path.abspath(output_file_name)
+    input_file_name = sys.argv[1]
+    input_file_path = os.path.abspath(input_file_name)
 
-if not os.path.exists(input_file_path):
-    sys.stderr.write(f"Missing {input_file_name}\n")
-    sys.exit(1)
+    output_file_name = sys.argv[2]
+    output_file_path = os.path.abspath(output_file_name)
 
-sys.exit(0)
+    if not os.path.exists(input_file_path):
+        sys.stderr.write(f"Missing {input_file_name}\n")
+        sys.exit(1)
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
